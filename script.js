076d95fc3f8f93ae116d502b826b1e41643f1773
lynnvdbo <lynn.van.den.bosch@hva.nl>
2025-12-04 21:15:52 +0100

    java script die ervoor zorgt dat alle buttons met dezelfde class geliked kunnen worden en gedisliked  #18

--- script.js
+++ script.js
@@ -20,3 +20,31 @@
 
 
 
+
+
+
+
+
+
+
+/* deze zorgt evrvoor dat je alles buttons kan liken en ze blijven geliked */
+
+
+
+/* selecteerd alle buttons met de class .leeg */
+let likeButtons = document.querySelectorAll(".leeg");
+
+/* hij loopt door alle buttons heen */
+likeButtons.forEach(function(button){
+    /* als iemand op een van de like buttons clickt gebeurt er een function */
+    button.addEventListener("click", function(){
+        /* function is togglen met voor alle classes met fill*/
+        button.classList.toggle("fill")
+    })
+})
+
+
+
+
+
+ 
