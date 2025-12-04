3d333ae39c0a11b58281e9d87620ec872d421b81
lynnvdbo <lynn.van.den.bosch@hva.nl>
2025-12-04 21:14:08 +0100

    java script die ervoor zorgt dat je 1 button kan liken en disliken #18

---
+++ script.js
@@ -0,0 +1,22 @@
+/* deze zorgt ervoor dat je 1 button kan liken, maar nog niet andere */
+
+
+
+/* STAP 1: zoek de button op - document.querySelector() */
+// let likeButton = document.querySelectorAll(".leeg");
+
+
+/* STAP 2: als iemand er op clickt - addEventListener() */
+// likeButton.addEventListener("click", toggleLike);
+
+
+/* STAP 3: hier geef je aan wat de functie is*/
+// function toggleLike () {
+
+/* hartje geef je aan welke class hij gaat togglen, in dit geval de class fill  */
+// 	likeButton.classList.toggle("fill");
+
+
+
+
+
